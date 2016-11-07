a174649a181f9d9adb9dcaa9984e304476ef546f
Narayanan Sundaram <narayanan.sundaram@intel.com>
2016-11-07 15:09:37 -0800

    add copyright notice

--- test/graph_test.cpp
+++ test/graph_test.cpp
@@ -1,3 +1,34 @@
+/******************************************************************************
+** Copyright (c) 2015, Intel Corporation                                     **
+** All rights reserved.                                                      **
+**                                                                           **
+** Redistribution and use in source and binary forms, with or without        **
+** modification, are permitted provided that the following conditions        **
+** are met:                                                                  **
+** 1. Redistributions of source code must retain the above copyright         **
+**    notice, this list of conditions and the following disclaimer.          **
+** 2. Redistributions in binary form must reproduce the above copyright      **
+**    notice, this list of conditions and the following disclaimer in the    **
+**    documentation and/or other materials provided with the distribution.   **
+** 3. Neither the name of the copyright holder nor the names of its          **
+**    contributors may be used to endorse or promote products derived        **
+**    from this software without specific prior written permission.          **
+**                                                                           **
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS       **
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT         **
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR     **
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      **
+** HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,    **
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED  **
+** TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR    **
+** PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    **
+** LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING      **
+** NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS        **
+** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* ******************************************************************************/
+/* Narayanan Sundaram (Intel Corp.)
+ * ******************************************************************************/
+
 #include "catch.hpp"
 #include "generator.hpp"
 #include <algorithm>
@@ -33,16 +64,20 @@ void test_graph(int n) {
     if (G.vertexNodeOwner(i)) {
       custom_vertex_type v;
       v.iprop = i;
-      v.fprop = i*2.5f;
+      v.fprop = i*2.5;
       G.setVertexproperty(i, v);
     }
   }
 
   for (int i = 1; i <= n; i++) {
-    if (G.vertexNodeOwner(i)) 
+    if (G.vertexNodeOwner(i)) {
       REQUIRE(G.getVertexproperty(i).iprop == i);
-      REQUIRE(G.getVertexproperty(i).fprop == Approx(2.5f*i));
+      REQUIRE(G.getVertexproperty(i).fprop == Approx(2.5*i));
+    }
   }
+
+  std::string fname = "/dev/shm/vprop.txt";
+  G.saveVertexproperty(fname);
 
 }
 
