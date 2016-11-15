9b6d9e4c8e2e226da152c581b3d15742ccf84b51
Narayanan Sundaram <narayanan.sundaram@intel.com>
2016-11-15 14:50:46 -0800

    add vector generators

--- test/generator.hpp
+++ test/generator.hpp
@@ -137,4 +137,58 @@ GraphPad::edgelist_t<T> generate_dense_edgelist(int n) {
   return dense_edgelist;
 }
 
+
+//////////////////////////////////////////
+// Sparse Vector generators //////////////
+//////////////////////////////////////////
+
+
+template <typename T=int>
+GraphPad::edgelist_t<T> generate_dense_vector_edgelist(int n) {
+  int global_myrank = GraphPad::get_global_myrank();
+  GraphPad::edgelist_t<T> dense_edgelist(n, 1, 0);
+  if (global_myrank == 0) {
+    dense_edgelist = GraphPad::edgelist_t<T>(n, 1, n); 
+    for (int i = 0; i < n; i++) {
+      dense_edgelist.edges[i].src = i+1;
+      dense_edgelist.edges[i].dst = 1;
+      dense_edgelist.edges[i].val = 1;
+    }
+  }
+  return dense_edgelist;
+}
+
+template <typename T=int>
+GraphPad::edgelist_t<T> generate_random_vector_edgelist(int n, int nnz) {
+  if (nnz > n) { 
+    nnz = n;
+    return generate_dense_vector_edgelist<T>(n);
+  }
+
+  int global_myrank = GraphPad::get_global_myrank();
+  GraphPad::edgelist_t<T> random_edgelist(n, 1, 0);
+  if (global_myrank == 0) {
+    random_edgelist = GraphPad::edgelist_t<T>(n, 1, nnz); 
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(1, n);
+  
+    for (int j = 0; j < nnz; j++) {
+      random_edgelist.edges[j].src = 1;
+      int dst;
+      do {
+        dst = dist(gen);
+      } while(dst_present(dst, random_edgelist, 0, j));
+
+      random_edgelist.edges[j].dst = dst;
+      random_edgelist.edges[j].val = 1;
+    }
+
+    for (int j = 0; j < nnz; j++) {
+      std::swap(random_edgelist.edges[j].src, random_edgelist.edges[j].dst);
+    }
+  }
+  return random_edgelist;
+}
+
 #endif 
